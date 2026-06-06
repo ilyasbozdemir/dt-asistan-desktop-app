d718448657837e1456b475eb29558e46be4c3e89
İlyas Bozdemir <bozdemir.ib70@gmail.com>
2026-06-07 01:00:58 +0300

    feat: add EskiDosyaKopyalaModal component for selecting and copying existing file templates

--- src/renderer/src/screens/dosyalar/components/EskiDosyaKopyalaModal.tsx
+++ src/renderer/src/screens/dosyalar/components/EskiDosyaKopyalaModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { X, Search, FileText, Calendar, Copy, ChevronRight } from 'lucide-react'
 import { TeminDosyasi } from '../dosyalar.hooks'
 import { cn } from '../../../utils/cn'
